966931e1d8bb0a585132b75df053c3fe97cf0e26
Alexander <ixav1@icloud.com>
2018-09-27 07:44:39 +0300

    class caps net

--- caps_net.py
+++ caps_net.py
@@ -1,11 +1,102 @@
 import numpy as np
 import tensorflow as tf
 
+#Small constant used in squash function to avoid division by zero.
+SQAUSH_CONST = 1e-10
+CONV_STDDEV = 0.1
+DTYPE_F = tf.float32
+DTYPE_INT = tf.int32
+
+class CapsNet(object):
+  def __init__(self,
+               input_dim,
+               conv_layer=(3, 16, 1),
+               conv_activaion=tf.nn.relu,
+               primary_caps=(3, 16, 8, 2)):
+    '''
+    Initialize CapsNet.
+    Args:
+      input_dim: size of input data, tuple.
+    '''
+    self.X = tf.placeholder(DTYPE_F, shape=(None,)+input_dim)
+    self.Y = tf.placeholder(DTYPE_INT, shape=(None))
+
+    #Creating convolutional layer.
+    conv_kernel_size = conv_layer[0]
+    conv_input_layers = input_dim[-1]
+    conv_f_maps = conv_layer[1] #Number of convolutional future maps.
+    conv_stride = conv_layer[2]
+    conv = create_conv(x, (conv_kernel_size, conv_kernel_size, conv_input_layers, conv_f_maps),
+      stride=conv_stride, activation=conv_activaion, name='conv')
+
+    #Creating PrimaryCaps (PC) layer.
+    #PrimaryCaps layer is similar to convolutional layer.
+    pc_kernel_size = primary_caps[0]
+    pc_input_layers = conv[-1]
+    pc_num_capsules = primary_caps[1] #Number of PrimaryCaps layer capsules.
+    pc_out_vec_dim = primary_caps[2] #Dimension of output vector from one primary capsule.
+    pc_f_maps = pc_num_capsules*pc_out_vec_dim #Total number of future maps of PrimaryCaps layer.
+    pc_stride = primary_caps[3]
+    primary_capsules = create_conv(conv, (pc_kernel_size, pc_kernel_size, pc_input_layers, pc_f_maps),
+      [1, pc_stride, pc_stride, 1], 'primary_caps')
+    # sss = tf.shape(capsules)[-2]*tf.shape(capsules)[-3]*NUM_CAPSULES
+    # capsules_out = tf.reshape(capsules, (-1, sss, VEC_LEN, 1))
+
+
+
+
+
+
+def create_conv(input, kernel, stride=1, activation=tf.nn.relu,
+                padding='SAME', name='conv_layer'):
+  '''
+    Create convolutional layer.
+    Args:
+      input
+
+    Returns:
+      A squashed vector with the same shape as input vector.
+  '''
+  with tf.name_scope(name):
+    stride = (1, stride, stride, 1)
+    conv_kernel_init = tf.truncated_normal(kernel, dtype=DTYPE_F, stddev=CONV_STDDEV)
+    conv_kernel = tf.Variable(conv_kernel_init, name='weights_'+name)
+    conv_bias = tf.Variable(tf.constant(0.0, shape=[kernel_shape[-1]], dtype=DTYPE_F),
+      trainable=True, name='biases_'+name)
+    conv = tf.nn.conv2d(inpt, conv_kernel, stride, padding=padding)
+    if activation == None:
+      return tf.nn.bias_add(conv, conv_bias)
+    else:
+      return activation(tf.nn.bias_add(conv, conv_bias))
+
+def squash(vector):
+  '''
+    Squash function.
+    Args:
+      vector: a tensor with shape [batch_size, num_caps, caps_out_vec_len, 1] or
+       [batch_size, num_caps, 1, caps_out_vec_len, 1].
+    Returns:
+      A squashed vector with the same shape as input vector.
+  '''
+  squared_vector_norm = tf.reduce_sum(tf.square(vector), -2, keepdims=True)
+  vector_norm = tf.sqrt(squared_vector_norm)
+  scaling_factor = squared_vector_norm / (1 + squared_vector_norm)
+  vector_squashed = scaling_factor * vector / (vector_norm + SQAUSH_CONST)
+  return vector_squashed
+
+
+
+
+
+
+
+
 mnist = tf.keras.datasets.mnist
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
 print(x_train.shape)
 print(y_train)
+
 
 x = tf.placeholder(tf.float32, shape=(None, 28, 28, 1))
 y = tf.placeholder(tf.int32, shape=(None))
@@ -132,7 +223,7 @@ print(sq_capsules_res[0].shape)
 
 print('====')
 
-print(sess.run(tf.shape(u_produce_v), feed_dict={x: x_batch}))
+print(sess.run(tf.shape(conv1), feed_dict={x: x_batch}))
 # print(sess.run(tf.shape(W), feed_dict={x: x_batch}))
 # print(sess.run(tf.shape(input), feed_dict={x: x_batch}))
 print(sess.run(tf.shape(v_J), feed_dict={x: x_batch}))
